2fa7a9c83f25422906b3841c1fa59fbefb723336
Victor <carvajal57685@gmail.com>
2024-07-22 09:45:22 -0500

    First commit

---
+++ src/TaskWeb.jsx
@@ -0,0 +1,9 @@
+
+
+export const TaskWeb = () => {
+  return (
+    <>
+        <h1>TaskWeb</h1>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { TaskWeb } from './TaskWeb'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TaskWeb />
+  </React.StrictMode>,
+)
